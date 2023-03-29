34d94f02ee7f8fd685a2c090c573dd94c7c02194
CebotariTigran <cebotaritigran@gmail.com>
2023-03-29 21:19:22 +0200

    Add https

--- src/getData.js
+++ src/getData.js
@@ -2,9 +2,9 @@ import { showCurrentData } from "./showData";
 
 export async function getWeather(place) {
     try {
-        const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=829d734e92bc4a00882163051232803&q=' + place + '&days=7&aqi=no&alerts=no')
+        const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=829d734e92bc4a00882163051232803&q=' + place + '&days=7&aqi=no&alerts=no')
         const weatherData = await response.json()
-        const responseAstronomy = await fetch('http://api.weatherapi.com/v1/astronomy.json?key=829d734e92bc4a00882163051232803&q=' + place + '&dt=')
+        const responseAstronomy = await fetch('https://api.weatherapi.com/v1/astronomy.json?key=829d734e92bc4a00882163051232803&q=' + place + '&dt=')
         const astronomyData = await responseAstronomy.json()
         showCurrentData(weatherData, astronomyData)
         console.log(weatherData.forecast.forecastday[0].hour[0])
